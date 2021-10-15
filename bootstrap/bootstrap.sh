8c212b338e2dcea7b143e8635bd4479d091d3167
b97pla <pontus.larsson@medsci.uu.se>
2021-10-15 16:54:51 +0200

    bootstrapping for miarka

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-set -x 
+set -x
+set -e
 
-# Minimalistic and hard coded script for bootstrapping the Ansible environment on irma3
+# Minimalistic and hard coded script for bootstrapping the Ansible environment on miarka3
+PROVISIONURL=https://github.com/NationalGenomicsInfrastructure/miarka-provision/archive/refs/heads
+PROVISIONBRANCH=devel
+VIRTUALENVURL=https://pypi.python.org/packages/source/v/virtualenv
+VIRTUALENVVERSION=20.8.1
+REPOPATH="$HOME/vulpes/ngi/miarka3"
 
-if [ -d /lupus/ngi/irma3/deploy ] ; then
-	echo "Irma provisioning repo already exists, so the irma3 environment is probably already setup properly. Aborting."
-	exit 1
+if [ -d  "$REPOPATH/deploy" ] ; then
+        echo "Miarka provisioning repo already exists, so the miarka3 environment is probably already setup properly. Aborting."
+        exit 1
 fi
 
 umask 0002
 
-mkdir -p /lupus/ngi/irma3/
-chgrp ngi-sw /lupus/ngi/irma3
-chmod g+rwXs /lupus/ngi/irma3
+mkdir -p $REPOPATH/
+chgrp ngi-sw $REPOPATH
+chmod g+rwXs $REPOPATH
 
-echo "Cloning the Irma provisioning repo" 
-git clone https://github.com/NationalGenomicsInfrastructure/irma-provision /lupus/ngi/irma3/deploy
+echo "Cloning the Miarka provisioning repo"
+ZIPNAME="miarka-provision-${PROVISIONBRANCH}"
+curl -L -o "$REPOPATH/${ZIPNAME}.zip" "$PROVISIONURL/${PROVISIONBRANCH}.zip"
+unzip -d $REPOPATH "$REPOPATH/${ZIPNAME}.zip"
+mv "$REPOPATH/${ZIPNAME}" $REPOPATH/deploy
 
-mkdir -p /lupus/ngi/irma3/devel/
-mkdir -p /lupus/ngi/irma3/log/
+mkdir -p $REPOPATH/devel/
+mkdir -p $REPOPATH/log/
 
-echo "Copying environment bashrc file" 
-cp /lupus/ngi/irma3/deploy/bootstrap/bashrc /lupus/ngi/irma3/bashrc
+echo "Copying environment bashrc file"
+cp $REPOPATH/deploy/bootstrap/bashrc $REPOPATH/bashrc
 
-echo "Downloading Python virtualenv" 
-curl -o /lupus/ngi/irma3/virtualenv-15.0.0.tar.gz https://pypi.python.org/packages/source/v/virtualenv/virtualenv-15.0.0.tar.gz 
-
-cd /lupus/ngi/irma3 && tar xfz virtualenv-15.0.0.tar.gz
-
-echo "Setting up a virtual environment for Ansible"
-/lupus/ngi/irma3/virtualenv-15.0.0/virtualenv.py -p /usr/bin/python2.7 /lupus/ngi/irma3/ansible-env
-
-/lupus/ngi/irma3/ansible-env/bin/activate
+echo "Setting up a venv for Ansible"
+/usr/bin/python3 -m venv "$REPOPATH/ansible-env"
+source "$REPOPATH/ansible-env/bin/activate"
+pip install --upgrade pip
 
 echo "Installing Ansible into virtual environment"
 pip install ansible
@@ -41,6 +45,6 @@ echo "Installing pexpect for syncing functionality into the cluster"
 pip install pexpect
 
 echo "Installing cpanm so we can install Perl packages locally"
-cd /lupus/ngi/irma3/ansible-env/bin
+cd $REPOPATH/ansible-env/bin
 curl -L https://cpanmin.us -o cpanm
 chmod +x ./cpanm
